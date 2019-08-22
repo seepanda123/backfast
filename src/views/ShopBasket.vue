ff3550986bc8866da53fe8bc6c7452788081f581
d176 <1538208614@qq.com>
2019-08-22 16:51:12 +0800

    pro

--- src/views/ShopBasket.vue
+++ src/views/ShopBasket.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="Body">
     <header>
-      <van-nav-bar title="菜篮子" right-text="清空" @click-right="tap()" :fixed="true"/>
-
+      <van-nav-bar title="菜篮子" right-text="清空" @click-right="tap()" :fixed="true" />
     </header>
     <section>
-<!-- <van-nav-bar title="菜篮子" right-text="清空" @click-right="tap()" :fixed="true"/> -->
-        <van-tabs @click="onClick" title-active-color="#FF0" sticky scrollTop= 46px >
+      <!-- <van-nav-bar title="菜篮子" right-text="清空" @click-right="tap()" :fixed="true"/> -->
+      <van-tabs @click="onClick" title-active-color="#FF0" sticky scrollTop="46px">
         <van-tab title="按菜品查看">
           <div>
             <h3>
@@ -21,86 +20,31 @@
             </h3>
             <ul>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
             </ul>
           </div>
-
-
-
-
-
-
-           <div>
-            <h3>
-              <van-image
-                width="50"
-                height="50"
-                src="https://img.yzcdn.cn/vant/cat.jpeg"
-                class="Image"
-              />
-              <p style="display:inline-block">鲜荷叶百合莲子粥</p>
-              <span @click="remove()" style="color:#ff0">移出这个菜</span>
-            </h3>
-            <ul>
-              <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
-                </van-row>
-              </li>
-              <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
-                </van-row>
-              </li>
-              <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
-                </van-row>
-              </li>
-              <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
-                </van-row>
-              </li>
-            </ul>
-          </div>
-
-
-
           <div>
             <h3>
               <van-image
@@ -114,71 +58,128 @@
             </h3>
             <ul>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
             </ul>
           </div>
-</van-tab>
 
-        <van-tab title="按材料查看">闪电风暴风格不符规范规划发广告符合你规范那个号发过火
-           <ul>
+          <div>
+            <h3>
+              <van-image
+                width="50"
+                height="50"
+                src="https://img.yzcdn.cn/vant/cat.jpeg"
+                class="Image"
+              />
+              <p style="display:inline-block">鲜荷叶百合莲子粥</p>
+              <span @click="remove()" style="color:#fafafa">移出这个菜</span>
+            </h3>
+            <ul>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
               <li>
-                <van-row gutter="20">
-                  <van-col span="8">鲜荷叶</van-col>
-                  <van-col span="8"></van-col>
-                  <van-col span="8">2个</van-col>
+                <van-row>
+                  <van-col span="20">鲜荷叶</van-col>
+                  <van-col span="4">2个</van-col>
                 </van-row>
               </li>
             </ul>
+          </div>
         </van-tab>
-        </van-tabs>
 
+        <van-tab title="按材料查看">
+         <h4>姜</h4>
+          <ul>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+          </ul>
+          <h4>水淀粉</h4>
+          <ul>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+          </ul>
+
+            <h4>清水</h4>
+          <ul>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+             <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+            <li>
+              <van-row>
+                <van-col span="20">鲜荷叶</van-col>
+                <van-col span="4">2个</van-col>
+              </van-row>
+            </li>
+          </ul>
+        </van-tab>
+      </van-tabs>
+      <router-view></router-view>
     </section>
   </div>
 </template>
@@ -198,38 +199,45 @@ export default {
     onClick(name, title) {
       this.$toast(title);
     },
-    remove(){
-      console.log(11)
+    remove() {
+      console.log(11);
     }
   }
 };
 </script>
 
 <style scoped>
-li {
+h3 {
   height: 100px;
+  margin-left:5vw;
+}
+h3 span {
+  margin-left: 150px;
 }
 .Image {
   float: left;
-  padding: 25px;
-}
-section ul {
-  margin-top: 5vh;
+  padding: 5px;
 }
 section li {
-  margin-left: 5vw;
+  height: 100px;
+  background: #f5f1f1;
+  margin: 10px 25px;
+  line-height: 100px;
 }
-Body{
-  height:100vh;
+
+Body {
+  height: 100vh;
   display: flex;
   flex-direction: column;
-  }
-  section{
-    flex:1;
-    overflow: hidden;
-    margin-top:46px;
-  }
- .tab{position: relative;
-    top: 46px;}
-
+}
+section {
+  flex: 1;
+  overflow:auto;
+  margin: 46px 0;
+}
+h4{
+  height: 40px;
+  line-height: 40px;
+  margin-left:30px;
+}
 </style>
